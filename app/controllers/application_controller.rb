e83e1213ed372f8de06b9e4cd3c326ded7f66732
Varun Kumar <57076268+varkum@users.noreply.github.com>
2022-08-06 12:19:48 -0400

    flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
     def set_current_user
+        flash[:alert] = nil
+        flash[:notice] = "This is a notice!"
         if session[:user_id]
             Current.user = User.find_by(id: session[:user_id])
         end
@@ -7,5 +10,10 @@ class ApplicationController < ActionController::Base
 
     def require_user_logged_in!
         redirect_to signin_path, alert: "Please login to continue." if Current.user.nil?
+    end
+
+    def clear_messages
+        flash[:alert] = nil
+        flash[:notice] = nil
     end
 end
