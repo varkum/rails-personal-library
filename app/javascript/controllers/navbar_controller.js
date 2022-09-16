3dd229f9bb2b80f3841ff9c90724fb0db5afe6d0
Varun Kumar <57076268+varkum@users.noreply.github.com>
2022-09-16 13:09:43 -0400

    mobile view books

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["menu", "button"];
+  static targets = ["menu"];
 
   open() {
     this.menuTarget.classList.toggle("hidden");
