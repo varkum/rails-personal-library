2cacbfefbb0b6c284062a71c694435a621335b69
Varun Kumar <57076268+varkum@users.noreply.github.com>
2022-09-28 14:26:46 -0400

    mobile fixes all

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def conditional_link_to(condition, url, style, &content)
+        if condition
+            return link_to url, target: "_top", class: style, &content 
+        else 
+            return link_to nil, class: style, &content
+        end
+    end
 end
--- app/helpers/podcasts_helper.rb
+++ app/helpers/podcasts_helper.rb
@@ -1,2 +1,3 @@
 module PodcastsHelper
+   
 end
